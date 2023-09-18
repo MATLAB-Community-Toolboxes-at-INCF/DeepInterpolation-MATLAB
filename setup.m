01e8c2965a5786fef25641916acee2016595cefe
Joris <joris.bentvelsen@vortech.nl>
2023-09-18 15:17:37 +0200

    Make scripts run independent of base folder

--- setup.m
+++ setup.m
@@ -1,8 +1,8 @@
-function setup()
+function base_folder = setup()
     % Add current directory and sample_data, layers and exmamples to path
-    setup_path = fileparts(mfilename('fullpath'));
-    addpath(setup_path);
-    addpath(fullfile(setup_path, "network_layers"))
-    addpath(fullfile(setup_path, "sample_data"))
-    addpath(fullfile(setup_path, "examples"))
+    base_folder = fileparts(mfilename('fullpath'));
+    addpath(base_folder);
+    addpath(fullfile(base_folder, "network_layers"))
+    addpath(fullfile(base_folder, "sample_data"))
+    addpath(fullfile(base_folder, "examples"))
 end
