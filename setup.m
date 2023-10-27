bce34bfccc33c8061f2a4195417ad777fa9556e4
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2023-10-27 08:16:35 -0400

    fixed typo

--- setup.m
+++ setup.m
@@ -1,5 +1,5 @@
 function base_folder = setup()
-    % Add current directory and sample_data, layers and exmamples to path
+    % Add current directory and sample_data, layers and examples to path
     base_folder = fileparts(mfilename('fullpath'));
     addpath(base_folder);
     addpath(fullfile(base_folder, "network_layers"))
